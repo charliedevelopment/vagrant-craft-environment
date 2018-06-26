bc1d275d9a12efca44f37495e2a514d5df319720
Sebastian Bossarte <kommentgames@gmail.com>
2018-06-26 15:41:00 -0500

    Wrong switch on install Postgre script.

--- setup/use-postgresql.sh
+++ setup/use-postgresql.sh
@@ -30,7 +30,7 @@ sudo systemctl stop mariadb
 sudo systemctl disable mariadb
 # Only run install/initial setup if PostgreSQL isn't currently installed
 yum list installed postgresql-server
-if [[ $? == 0 ]]; then
+if [[ $? == 1 ]]; then
 	sudo yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 	# Install PostgreSQL
 	sudo yum install -y postgresql96
